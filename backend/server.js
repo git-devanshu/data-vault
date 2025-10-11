d535f09ed21115318d3cd0f889ea1839dd0d7614
git-devanshu <devanshulanjudkar9@gmail.com>
2025-10-11 22:29:39 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const { taskRouter } = require('./routes/taskRouter');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.ORIGIN,
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded());
 
